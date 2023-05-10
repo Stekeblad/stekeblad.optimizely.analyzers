0b4ed091598e3b55d1501a5b28307538eb415fd9
Stekeblad <12627375+Stekeblad@users.noreply.github.com>
2023-05-10 19:24:21 +0200

    Changed documentation links in the analyzers to point to the file version on GitHub tagged with the installed version of Stekeblad.Optimizely.Analyzers

--- src/Analyzers/Analyzers/MyDiagnosticAnalyzerBase.cs
+++ src/Analyzers/Analyzers/MyDiagnosticAnalyzerBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.CodeAnalysis.Diagnostics;
+using System;
 using System.Diagnostics;
 using System.Reflection;
 
@@ -7,25 +8,9 @@ namespace Stekeblad.Optimizely.Analyzers.Analyzers
 	public abstract class MyDiagnosticAnalyzerBase : DiagnosticAnalyzer
 	{
 		protected static string HelpUrl(string diagnosticsId)
-			=> $"https://github.com/Stekeblad/stekeblad.optimizely.analyzers/blob/master/doc/Analyzers/{diagnosticsId}.md";
-
-		protected static string HelpUrl(string diagnosticsId, bool addVersion)
 		{
-			if (!addVersion)
-				return HelpUrl(diagnosticsId);
-
 			var version = GetVersion();
-
-			if (!version.Equals("1.0.0"))
-			{
-				// Release build, link to documentation at the tag for the installed version
-				return $"https://github.com/Stekeblad/stekeblad.optimizely.analyzers/tree/v{version}/doc/Analyzers/{diagnosticsId}.md";
-			}
-			else
-			{
-				// Dev build, link to the latest version of the documentation
-				return HelpUrl(diagnosticsId);
-			}
+			return $"https://github.com/Stekeblad/stekeblad.optimizely.analyzers/blob/{version}/doc/Analyzers/{diagnosticsId}.md";
 		}
 
 		private static string _version = null;
@@ -35,7 +20,9 @@ namespace Stekeblad.Optimizely.Analyzers.Analyzers
 			{
 				Assembly assembly = Assembly.GetExecutingAssembly();
 				FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
-				_version = fileVersionInfo.ProductVersion;
+				_version = fileVersionInfo.ProductVersion.Equals("1.0.0")
+					? "master" // Dev build, link to the latest version of the documentation
+					: $"v{fileVersionInfo.ProductVersion}"; // Release build, link to documentation at the tag for the installed version
 			}
 			return _version;
 		}
